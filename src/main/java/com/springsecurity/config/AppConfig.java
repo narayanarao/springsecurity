e25c2cb29c545865a5ef749ac69e36f58d677130
unknown <narayanarao429@gmail.com>
2020-07-12 16:10:19 +0530

    Initial commit

---
+++ src/main/java/com/springsecurity/config/AppConfig.java
@@ -0,0 +1,36 @@
+package com.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "com.springsecurity.*" })
+@Import({ SecurityConfig.class })
+public class AppConfig {
+
+	@Bean(name = "dataSource")
+	public DriverManagerDataSource dataSource() {
+	    DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+	    driverManagerDataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+	    driverManagerDataSource.setUrl("jdbc:sqlserver://192.168.1.50:1433;database=TEST2014");
+	    driverManagerDataSource.setUsername("sa");
+	    driverManagerDataSource.setPassword("developer@2015");
+	    return driverManagerDataSource;
+	}
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/pages/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
